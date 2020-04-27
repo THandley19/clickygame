6ad657d095504436d7e14179bfd33553ac280219
Trevor <marketing@southernoaksinnstaug.com>
2020-04-26 22:05:27 -0400

    starting files

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import Images from "./components/images";
+
+import "./App.css";
+
+class App extends Component {
+  render() {
+    return <Images />;
+  }
+}
+
+export default App;
